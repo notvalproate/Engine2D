d5d2fa15ac5412699b2584ca296b1cb2aeb22b0f
Ian Tauro <sushruth127@gmail.com>
2023-04-13 21:49:22 +0530

    Added background and foreground tilemap support

--- src/Tilemap.cpp
+++ src/Tilemap.cpp
@@ -1,30 +1,41 @@
 #include "Tilemap.hpp"
 #include "TextureLoader.hpp"
 
-Tilemap::Tilemap(unsigned short* p_TileMap, const char* p_TilesPath, const std::vector<SDL_Rect>& p_Tiles, SDL_Renderer* p_Renderer) {
-	m_Map = p_TileMap;
-	m_Tiles = p_Tiles;
+Tilemap::Tilemap(unsigned short* p_FgTileMap, unsigned short* p_BgTileMap, const char* p_BgPath, const char* p_TilesPath, const std::vector<SDL_Rect>& p_Tiles, SDL_Renderer* p_Renderer) {
+	SDL_Texture* TilemapTex = TextureUtil::LoadTexture(p_TilesPath, p_Renderer); 
+	SDL_Texture* BgTex = TextureUtil::LoadTexture(p_BgPath, p_Renderer);
+
 	m_Renderer = p_Renderer;
-	m_TilemapTex = TextureUtil::LoadTexture(p_TilesPath, p_Renderer);
 	m_Buffer = SDL_CreateTexture(p_Renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_TARGET, 320, 180);
 
 	SDL_Rect Temp = { 0, 0, 8, 8 };
 
 	SDL_SetRenderTarget(p_Renderer, m_Buffer);
+
+	SDL_RenderCopy(p_Renderer, BgTex, NULL, NULL);
+	int k;
+
 	for (int i = 0; i < 23; i++) {
 		Temp.y = i * 8;
 		for (int j = 0; j < 40; j++) {
 			Temp.x = j * 8;
-			if (p_TileMap[(i * 40) + j] == 0) {
-				continue;
+			k = (i * 40) + j;
+
+			if (p_FgTileMap[k] != 0) {
+				SDL_RenderCopy(p_Renderer, TilemapTex, &p_Tiles[p_FgTileMap[k] - 1], &Temp);
 			}
-			SDL_RenderCopy(p_Renderer, m_TilemapTex, &m_Tiles[p_TileMap[(i * 40) + j] - 1], &Temp);
+			if (p_BgTileMap[k] != 0) {
+				SDL_RenderCopy(p_Renderer, TilemapTex, &p_Tiles[p_BgTileMap[k] - 1], &Temp);
+			}
 		}
 	}
+
 	SDL_SetRenderTarget(p_Renderer, NULL);
 }
 
-Tilemap::~Tilemap() { }
+Tilemap::~Tilemap() { 
+	SDL_DestroyTexture(m_Buffer);
+}
 
 void Tilemap::Render(){
 	SDL_RenderCopy(m_Renderer, m_Buffer, NULL, NULL);
--- src/Tilemap.hpp
+++ src/Tilemap.hpp
@@ -4,13 +4,11 @@
 
 class Tilemap {
 public:
-	Tilemap(unsigned short* p_TileMap, const char* p_TilesPath, const std::vector<SDL_Rect>& p_Tiles, SDL_Renderer* p_Renderer);
+	Tilemap(unsigned short* p_FgTileMap, unsigned short* p_BgTileMap, const char* p_BgPath, const char* p_TilesPath, const std::vector<SDL_Rect>& p_Tiles, SDL_Renderer* p_Renderer);
 	~Tilemap();
 
 	void Render();
 private:
-	unsigned short* m_Map;
-	std::vector<SDL_Rect> m_Tiles;
-	SDL_Texture *m_TilemapTex, *m_Buffer;
+	SDL_Texture *m_Buffer;
 	SDL_Renderer* m_Renderer;
 };
