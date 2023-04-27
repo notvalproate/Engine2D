8654deab441a54c1b35d9119e89c27b161c155a6
Ian Tauro <sushruth127@gmail.com>
2023-04-27 18:33:16 +0530

    Added comments to vector2d class

--- src/math/Vector2d.cpp
+++ src/math/Vector2d.cpp
@@ -7,24 +7,24 @@ Vector2d::Vector2d()
 Vector2d::Vector2d(const double& p_x, const double& p_y)
 	: x(p_x), y(p_y) { }
 
-Vector2d::~Vector2d() {
-
-}
-
 void Vector2d::Normalize() {
+	//If either x and y is 0, return;
 	if (x == 0 && y == 0) {
 		return;
 	}
+	//Get the magnitude and divide x and y by it
 	double k = std::sqrt(x * x + y * y);
 	x /= k;
 	y /= k;
 }
 
 void Vector2d::Scale(const int& p_Factor) {
+	//Scale the vector in same direction by a factor
 	x *= p_Factor;
 	y *= p_Factor;
 }
 
 double Vector2d::GetMagnitude() {
+	//Return magnitude
 	return std::sqrt(x * x + y * y);
 }
--- src/math/Vector2d.h
+++ src/math/Vector2d.h
@@ -4,7 +4,6 @@ class Vector2d {
 public:
 	Vector2d();
 	Vector2d(const double& p_x, const double& p_y);
-	~Vector2d();
 
 	double x, y;
 
@@ -12,13 +11,20 @@ public:
 	void Scale(const int& p_Factor);
 	double GetMagnitude();
 
+	//Overloading basic operators
 	Vector2d operator+(const Vector2d& o_V) {
 		return Vector2d(x + o_V.x, y + o_V.y);
 	}
 	Vector2d operator-(const Vector2d& o_V) {
 		return Vector2d(x - o_V.x, y - o_V.y);
 	}
-	Vector2d operator/(const int& k) {
+	double operator*(const Vector2d& o_V) {
+		return x * o_V.x + y * o_V.y;
+	}
+	Vector2d operator/(const float& k) {
 		return Vector2d(x / k, y / k);
+	}
+	Vector2d operator*(const float& k) {
+		return Vector2d(x * k, y * k);
 	}
 };
